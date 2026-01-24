0c0da878a6c0c6ed68a8ff4d418b37f132d86265
Puffer-dev56 <pongsapat4848@gmail.com>
2026-01-24 20:46:03 +0700

    test

---
+++ Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveX;
+    private bool isJumping;
+    public float speed;
+    public float jumpForce;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveX * speed,rb.linearVelocity.y);
+        if (Input.GetButtonDown("Jump") && !isJumping) {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce)); 
+        }
+    }
+
+    //Checked Player on ground
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.CompareTag("Ground")) {
+            isJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D target) 
+    {
+        if (target.gameObject.CompareTag("Ground")) { 
+            isJumping = true;
+        }
+    }
+}
